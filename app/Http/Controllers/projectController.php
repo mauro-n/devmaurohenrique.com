bf739b7a635d2f35791f0f936234613c3de9f22d
test <mauro.henrique613@gmail.com>
2022-12-18 15:57:59 -0300

    linked controller with model

--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class projectController extends Controller
 {
     public function portfolio() {
+        $projects = Project::all();
+
         return view('portfolio');
     }
 }
